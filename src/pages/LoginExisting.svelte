b7e072da1f7546469829c659b0e1c37f95b6dba6
JohnWRalph <John.Ralph.Git@gmail.com>
2023-08-10 20:29:58 -0500

    removed shadow

--- src/pages/LoginExisting.svelte
+++ src/pages/LoginExisting.svelte
@@ -88,7 +88,7 @@
     }
 </script>
 
-<div style="box-shadow: 8px 18px 16px 0" class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+<divclass="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
     {#if $isLoggedIn}
         <div class="card-body">
             <div class="form-control mt-6">
